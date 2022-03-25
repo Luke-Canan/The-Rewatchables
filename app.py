9c714d61441b2c90662e4144afacf4fad5776721
Luke-Canan <luke.canan@gmail.com>
2022-03-25 07:11:19 -0500

    Configured Flask

--- app.py
+++ app.py
@@ -1,68 +1,105 @@
 from helpers import getAccessToken, getMovieTitle, getPodcastEpisodes, getStreamProviders
-from secrets import *
+from spotifyCodes import *
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-import csv
 import sqlite3
+from flask import Flask, redirect, render_template, request
 
-# Get Spotify API access token
-token = getAccessToken(clientID, clientSecret)
+app = Flask(__name__)
 
-# Get list of "The Rewatchables" podcast episodes
-podcastID = "1lUPomulZRPquVAOOd56EW"
-episodeList = getPodcastEpisodes(token, podcastID)
+@app.route("/", methods=["GET"])
+def index():
+    connection = sqlite3.connect("movie_db")
+    cursor = connection.cursor()
+    
+    try:
+        headers = cursor.execute("SELECT DISTINCT streamer FROM streamers ORDER BY UPPER(streamer)").fetchall()
+        return render_template("index.html", headers=headers)
+    except:
+        return redirect("/update")
 
-# Create list of movie titles
-movieList = []
-for episodeTitle in episodeList:
-    movie = getMovieTitle(episodeTitle)
-    if movie != None and movie not in movieList:
-        movieList.append(movie)
+@app.route("/movies", methods=["GET"])
+def movies():
 
-# Initialize Chrome webdriver
-CHROMEDRIVER_PATH = "/Applications/chromedriver"
-service = Service(CHROMEDRIVER_PATH)
-options = webdriver.ChromeOptions()
-options.headless = True
-driver = webdriver.Chrome(service=service, options=options)
+    provider = request.values.get("providers")
 
-# Collect streaming data for each movie
-streaming_data = []
-providers_list = []
-for movie in movieList:
-    providers = getStreamProviders(movie, driver)
-    for p in providers:
-        if p not in providers_list and p != "":
-            providers_list.append(p)
-    item = {
-        "movie": movie,
-        "providers": providers
-    }
-    streaming_data.append(item)
-    print(item)
+    connection = sqlite3.connect("movie_db")
+    cursor = connection.cursor()
 
-# End Chrome webdriver
-driver.quit()
+    movies = cursor.execute("SELECT movie_title FROM movies WHERE id IN (SELECT movie_id FROM streamers WHERE streamer = ?)", (provider,)).fetchall()
 
-# Initialize SQL
-connection = sqlite3.connect("movie_db")
-cursor = connection.cursor()
+    # movies = cursor.execute("SELECT movie_title FROM movies")
 
-# Create tables
-cursor.execute("CREATE TABLE movies (id INTEGER PRIMARY KEY, movie_title TEXT)")
-connection.commit()
-cursor.execute("CREATE TABLE streamers (movie_id INTEGER , streamer TEXT, FOREIGN KEY(movie_id) REFERENCES movies(id))")
-connection.commit()
+    return render_template("movies.html", movies=movies)
 
-# Populate database
-for movie in streaming_data:
-    cursor.execute("INSERT INTO movies (movie_title) VALUES(?)", (movie["movie"],))
+@app.route("/update", methods=["GET"])
+def update():
+
+    # Get Spotify API access token
+    token = getAccessToken(clientID, clientSecret)
+
+    # Get list of "The Rewatchables" podcast episodes
+    podcastID = "1lUPomulZRPquVAOOd56EW"
+    episodeList = getPodcastEpisodes(token, podcastID)
+
+    # Create list of movie titles
+    movieList = []
+    for episodeTitle in episodeList:
+        movie = getMovieTitle(episodeTitle)
+        if movie != None and movie not in movieList:
+            movieList.append(movie)
+
+    # Initialize Chrome webdriver
+    CHROMEDRIVER_PATH = "/Applications/chromedriver"
+    service = Service(CHROMEDRIVER_PATH)
+    options = webdriver.ChromeOptions()
+    options.headless = True
+    driver = webdriver.Chrome(service=service, options=options)
+
+    # Collect streaming data for each movie
+    streaming_data = []
+    providers_list = []
+    for movie in movieList:
+        providers = getStreamProviders(movie, driver)
+        for p in providers:
+            if p not in providers_list and p != "":
+                providers_list.append(p)
+        item = {
+            "movie": movie,
+            "providers": providers
+        }
+        streaming_data.append(item)
+        print(item)
+
+    # End Chrome webdriver
+    driver.quit()
+
+    # Initialize SQL
+    connection = sqlite3.connect("movie_db")
+    cursor = connection.cursor()
+
+    # Delete tables
+    cursor.execute("DROP TABLE movies")
     connection.commit()
-    # 
-    movie_id = cursor.lastrowid
-    for streamer in movie["providers"]:
-        cursor.execute("INSERT INTO streamers (movie_id, streamer) VALUES(?, ?)", (movie_id, streamer))
+    cursor.execute("DROP TABLE streamers")
+    connection.commit()
+
+    # Create tables
+    cursor.execute("CREATE TABLE movies (id INTEGER PRIMARY KEY, movie_title TEXT)")
+    connection.commit()
+    cursor.execute("CREATE TABLE streamers (movie_id INTEGER , streamer TEXT, FOREIGN KEY(movie_id) REFERENCES movies(id))")
+    connection.commit()
+
+    # Populate database
+    for movie in streaming_data:
+        cursor.execute("INSERT INTO movies (movie_title) VALUES(?)", (movie["movie"],))
         connection.commit()
-        
-# End connection
-connection.close()
+        movie_id = cursor.lastrowid
+        for streamer in movie["providers"]:
+            cursor.execute("INSERT INTO streamers (movie_id, streamer) VALUES(?, ?)", (movie_id, streamer))
+            connection.commit()
+
+    # End connection
+    connection.close()
+
+    return redirect("/")
